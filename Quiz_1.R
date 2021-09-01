2504459810eff8714b4bbc1e72bb39bcfdd5c053
millben <73913386+millben@users.noreply.github.com>
2021-09-01 12:20:07 -0400

    Update to Quiz_1

--- Quiz_1.R
+++ Quiz_1.R
@@ -1,3 +1,6 @@
+
+# Quiz 1
+
 library(tidyverse)
 str(iris)
 x=iris$Petal.Length
@@ -5,6 +8,20 @@ y=iris$Petal.Width
 c=iris$Species
 table(c)
 
+# Question 1 Method 1
 sub = subset(iris, Petal.Length>2.5 & Petal.Length<5 & Petal.Width >0.75 & Petal.Width <1.75)
 nrow(sub)
+ 
+# Question 1 Method 2
 
+sub = filter(iris, Petal.Length>2.5 & Petal.Length<5 & Petal.Width >0.75 & Petal.Width <1.75)
+nrow(sub)
+
+# Question 1 Method 3
+
+sub = iris %>% 
+  filter(Petal.Length>2.5) %>% 
+  filter(Petal.Length<5) %>% 
+  filter(Petal.Width>0.75) %>% 
+  filter(Petal.Width<1.75)
+nrow(sub)
